410b7cb10a23af6a8b52d8c4a8aac5a77a601cdf
yuehu1994 <yuehu1994@gmail.com>
2014-11-05 17:04:35 -0500

    Updated Code

--- app/src/main/java/com/example/yuehu/CameraActivity/Camera.java
+++ app/src/main/java/com/example/yuehu/CameraActivity/Camera.java
@@ -1,21 +1,41 @@
 package com.example.yuehu.CameraActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.yuehu.translator.MainActivity;
 import com.example.yuehu.translator.R;
-/**
- * Created by magpie on 11/4/14.
- */
+
+
 public class Camera extends Activity{
+
+    //Yue wrote this-rachel-change if you want!
+    private String inputString;
+    private String outputString;
+
+    public void backButton(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.camera_view);
+
+        //Yue also wrote this-should be ok:
+        Intent intent = getIntent();
+        String[] passedInfo = intent.getStringArrayExtra("MainActivity.MESSAGE");
+        inputString = passedInfo[0];
+        outputString = passedInfo[1];
+        ((TextView)findViewById(R.id.inputText)).setText("Translate: "+ inputString);
+        ((TextView)findViewById(R.id.outputText)).setText("To: "+ outputString);
     }
-
-
-//fuck this
 
 
 
--- app/src/main/java/com/example/yuehu/translator/MainActivity.java
+++ app/src/main/java/com/example/yuehu/translator/MainActivity.java
@@ -1,47 +1,110 @@
 package com.example.yuehu.translator;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import com.example.yuehu.CameraActivity.Camera;
+
+/**
+ * Yue Hu Wrote This File
+ */
 public class MainActivity extends Activity {
 
-    private String inputLanguage;
-    private String outputLanguage;
+    private String inputLanguage = "none";                               //these are global! watch out when changing
+    private String outputLanguage = "none";
+    public final static String EXTRA_MESSAGE = "MainActivity.MESSAGE";                   //pass this
 
     public String getInputLanguage(){
         return inputLanguage;
-    }
+    }               //getters
 
     public String getOutputLanguage(){
         return outputLanguage;
     }
 
-    public void setInputLanguage(String input){
-        inputLanguage = input;
-    }
-
-    public void setOutputLanguage(String output){
-        outputLanguage = output;
-    }
 
     public void translateSetClick(View view){
-        return;
+        switch(view.getId()){
+            case R.id.radioButtonEnglish:
+                if(inputLanguage.equals("none")){
+                    inputLanguage = "english";}                 //select
+                else if(inputLanguage.equals("english")){
+                    inputLanguage = "none";                     //deselect
+                }
+                break;
+            case R.id.radioButtonFrench:
+                if(inputLanguage.equals("none")){
+                    inputLanguage = "french";}                  //select
+                else if(inputLanguage.equals("french")){
+                    inputLanguage = "none";                     //deselect
+                }
+                break;
+            case R.id.radioButtonSpanish:
+                if(inputLanguage.equals("none")){
+                    inputLanguage = "spanish";}
+                else if(inputLanguage.equals("spanish")){
+                    inputLanguage = "none";
+                }
+                break;
+        }
+
     }
     public void toSetClick(View view){
-        return;
+        switch(view.getId()) {
+            case R.id.radioButtonEnglish:
+                if(outputLanguage.equals("none")){
+                    outputLanguage = "english";}                 //select
+                else if(outputLanguage.equals("english")){
+                    outputLanguage = "none";                     //deselect
+                }
+                break;
+            case R.id.radioButtonFrench:
+                if(outputLanguage.equals("none")){
+                    outputLanguage = "french";}                  //select
+                else if(outputLanguage.equals("french")){
+                    outputLanguage = "none";                     //deselect
+                }
+                break;
+            case R.id.radioButtonSpanish:
+                if(outputLanguage.equals("none")){
+                    outputLanguage="spanish";
+                }
+                else if(outputLanguage.equals("spanish")){
+                    outputLanguage = "none";
+                }
+                break;
+        }
     }
 
+
     public void toCamera(View view){
-        setContentView(R.layout.camera_view);
+        if(getInputLanguage().equals("none") || getOutputLanguage().equals("none")){
+            Context context = getApplicationContext();
+            CharSequence toastText = "Please select languages";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context,toastText,duration);
+            toast.show();
+        }
+        else{
+            Intent intent = new Intent(this, Camera.class);
+            String[] message = new String[]{inputLanguage,outputLanguage};      //simple parse in next view
+            intent.putExtra(EXTRA_MESSAGE, message);
+            startActivity(intent);
+        }
 
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);                                     //
         setContentView(R.layout.activity_main);
+        inputLanguage = "none";
+        outputLanguage = "none";
     }
 
 
