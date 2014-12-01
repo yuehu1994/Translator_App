86717d952cac90e9b3414d3516eacbbe27677f2e
yuehu1994 <yuehu1994@gmail.com>
2014-12-01 17:51:42 -0500

    updates for translation-does not work

--- app/src/main/java/com/example/yuehu/camera/camera_activity.java
+++ app/src/main/java/com/example/yuehu/camera/camera_activity.java
@@ -3,23 +3,35 @@ package com.example.yuehu.camera;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.content.ActivityNotFoundException;
 import com.example.yuehu.translator.R;
 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.StringTokenizer;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 //import com.google.api.translate.Language;
 //import com.google.api.translate.Translate;
 //import com.google.api.GoogleAPI;
 
-import com.memetix.mst.language.Language;
-import com.memetix.mst.translate.Translate;
+//import com.memetix.mst.language.Language;
+//import com.memetix.mst.translate.Translate;
 
 
 public class camera_activity extends Activity{
-
+    private String apiBrowserKey = "AIzaSyBZ4Ma9zIaBjTueqRD_pr9of3gsPwdyJ6I";
+    private String apiAndroidKey = "AIzaSyBPhcr6T60YYvDlRJIZQ5xDRMo4UrwkBsU";
     private String outputString;
     private String spokenString;
     private String translatedString;
@@ -28,26 +40,58 @@ public class camera_activity extends Activity{
     static final String STATE_TRANS="translatedString";
 
 
-    //WE NEED TO GET A KEY FOR THIS TO WORK
     public void translate(){
-
+        StringBuilder result = new StringBuilder();
+        InputStream myStream;
         try {
-            /*
-            THIS IS CURRENTLY BROKEN-EXCEPTION IS CAN"T GET TRANSLATED INFORMATION
-            I THINK THE GOOGLE API KEY I SET UP DOESN"T WORK RIGHT-WILL TRY TO FIX LATER.
-            I"M ON A 60 DAY FREE TRIAL FOR IT AND I THINK IM MISSING SOMETHING
-            ALSO I DONT KNOW WHAT SET HTTP REFERRER DOES BUT LOOKS FROM MY EXAMPLES YOU CAN PUT ANYTHING...
-             */
-
-  //          GoogleAPI.setHttpReferrer("http://www.google.com");
-  //          GoogleAPI.setKey("AIzaSyBPhcr6T60YYvDlRJIZQ5xDRMo4UrwkBsU");
-            Translate.setClientId("TranslatorYueHu");
-            Translate.setClientSecret("YlRnOgykvJljxs2oleRyp6wVW0M8HSV/goQXdYICI0s=");
-            translatedString=Translate.execute(spokenString, Language.ENGLISH, Language.FRENCH);
+/*
+            //GOOGLE TRANSLATE STUFF
+            GoogleAPI.setHttpReferrer("http://www.google.com");
+            GoogleAPI.setKey("AIzaSyBPhcr6T60YYvDlRJIZQ5xDRMo4UrwkBsU");
+            translatedString=Translate.DEFAULT.execute(spokenString, Language.ENGLISH, Language.FRENCH);//replace with getNewLanguage()
             ((TextView)findViewById(R.id.displayOutput)).setText(translatedString);
+*/
+
+            //BING TRANSLATE STUFF
+
+        /*
+            Translate.setClientId("YueTranslator");
+            Translate.setClientSecret("wgBD1d+s+in4cQyJGyZM+KVhr8E4o2I3Z8xY9+o0mQE=");
+            translatedString=Translate.execute(spokenString, Language.ENGLISH, Language.FRENCH);//replace with getNewLanguage()
+            ((TextView)findViewById(R.id.displayOutput)).setText(translatedString);
+
+        */
+
+        String newLanguage = getNewLanguage();
+        String encodedText = URLEncoder.encode(spokenString,"UTF-8");
+        String urlString = "https://www.googleapis.com/language/translate/v2?key=" +apiBrowserKey + "&source=en"+"&target=" + newLanguage+ "&q=" + encodedText;
+        //((TextView)findViewById(R.id.displayOutput)).setText(urlString);
+
+        URL url = new URL(urlString);
+        HttpsURLConnection connect = (HttpsURLConnection) url.openConnection();
+        //((TextView)findViewById(R.id.displayInput)).setText(Integer.toString(connect.getResponseCode()));
+        if(connect.getResponseCode() != 200) {    //Failed
+            String errorMessage = "Https Connection Failed";
+            Toast toast = Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT);
+            toast.show();
+            return;
+        }
+        myStream = connect.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(myStream));
+        String line;
+        while((line = reader.readLine())!= null){
+            result.append(line);
+        }
+        JsonParser myParser = new JsonParser();
+        JsonElement myElement = myParser.parse(result.toString());
+        JsonObject myObject= myElement.getAsJsonObject();
+        translatedString = myObject.get("data").getAsJsonObject().get("translations").getAsJsonArray().get(0).getAsJsonObject().get("translatedText").getAsString();//parses the  json
+        ((TextView)findViewById(R.id.displayOutput)).setText(translatedString);
+
+
         }
         catch(Exception e){
-            e.printStackTrace();
+            e.getMessage();//get rid of this line when done debugging
             String errorMessage = "Translation Error";
             Toast toast = Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT);
             toast.show();
@@ -56,22 +100,22 @@ public class camera_activity extends Activity{
 
     }
 
-    public Language getNewLanguage(){
-        Language newLanguage;
+    public String getNewLanguage(){
+        String newLanguage;
         if(outputString.equals("german")){
-            newLanguage = Language.GERMAN;
+            newLanguage = "de";
         }
         else if(outputString.equals("french")){
-            newLanguage = Language.FRENCH;
+            newLanguage = "fr";
         }
         else if(outputString.equals("spanish")){
-            newLanguage = Language.SPANISH;
+            newLanguage = "es";
         }
         else if(outputString.equals("chinese")){
-            newLanguage = Language.CHINESE_TRADITIONAL;
+            newLanguage = "zh_TW";
         }
         else if(outputString.equals("greek")){
-            newLanguage = Language.GREEK;
+            newLanguage = "el";
         }
         else {
             String errorMessage = "Language Selection Error";
